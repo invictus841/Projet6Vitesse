1dab8b08719947ee120d33c6e7c875f88f9a106a
invictus841 <usedforfunny@gmail.com>
2025-01-30 00:39:59 +0100

    Ajout du model User

---
+++ Projet6Vitesse/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Projet6Vitesse
+//
+//  Created by Alexandre Talatinian on 29/01/2025.
+//
+
+struct User: Codable {
+    let email: String
+    let password: String
+    let firstName: String
+    let lastName: String
+}
