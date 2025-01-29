713572cf5dfdb0478b44d1c79c9f6229da022d79
invictus841 <usedforfunny@gmail.com>
2025-01-30 00:46:44 +0100

    Ajout de AppStateViewModel qui gère les états isAdmin et isAuthenticated

---
+++ Projet6Vitesse/ViewModels/AppStateViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  AppStateViewModel.swift
+//  Projet6Vitesse
+//
+//  Created by Alexandre Talatinian on 29/01/2025.
+//
+
+import Foundation
+
+@MainActor
+class AppStateViewModel: ObservableObject {
+    @Published var isAuthenticated = false
+    @Published var isAdmin = false
+    
+    private let keychainService: KeychainServiceProtocol
+    
+    init(keychainService: KeychainServiceProtocol = KeychainService()) {
+        self.keychainService = keychainService
+    }
+    
+    func logout() {
+        try? keychainService.deleteToken()
+        isAuthenticated = false
+        isAdmin = false
+    }
+    
+//    func checkAuthStatus() {
+//        if let _ = try? keychainService.getToken() {
+//            isAuthenticated = true
+//        }
+//    }
+}
