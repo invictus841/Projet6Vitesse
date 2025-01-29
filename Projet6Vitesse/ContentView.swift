1700a4104e3a6dc4bf0794fea1d0bf8b98636f15
invictus841 <usedforfunny@gmail.com>
2025-01-29 21:28:14 +0100

    Initial Commit

---
+++ Projet6Vitesse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projet6Vitesse
+//
+//  Created by Alexandre Talatinian on 29/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projet6Vitesse/Projet6VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet6VitesseApp.swift
+//  Projet6Vitesse
+//
+//  Created by Alexandre Talatinian on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Projet6VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
