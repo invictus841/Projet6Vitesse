eb6bf65a108c65b893fd11d7c6de764f99a240a3
invictus841 <usedforfunny@gmail.com>
2025-01-30 00:48:20 +0100

    Ajout de MockKeychainService pour nous aider à faire des tests

---
+++ Projet6Vitesse/Mocks/MockKeychainService.swift
@@ -0,0 +1,22 @@
+//
+//  MockKeychainService.swift
+//  Projet6VitesseTests
+//
+//  Created by Alexandre Talatinian on 30/01/2025.
+//
+
+class MockKeychainService: KeychainServiceProtocol {
+    private var token: String?
+    
+    func save(token: String) throws {
+        self.token = token
+    }
+    
+    func getToken() throws -> String? {
+        return token
+    }
+    
+    func deleteToken() throws {
+        token = nil
+    }
+}
