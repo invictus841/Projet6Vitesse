c2f2c2957c9e97831b60d24c6e977f68a8468106
invictus841 <usedforfunny@gmail.com>
2025-01-30 00:47:46 +0100

    Ajout de MockAuthService pour nous permettre de faire des tests

---
+++ Projet6Vitesse/Mocks/MockAuthService.swift
@@ -0,0 +1,27 @@
+//
+//  MockAuthService.swift
+//  Projet6VitesseTests
+//
+//  Created by Alexandre Talatinian on 30/01/2025.
+//
+
+class MockAuthService: AuthServiceProtocol {
+    let shouldFail: Bool
+    
+    init(shouldFail: Bool = false) {
+        self.shouldFail = shouldFail
+    }
+    
+    func login(email: String, password: String) async throws -> AuthResponse {
+        if shouldFail {
+            throw NetworkError.unauthorized
+        }
+        return AuthResponse(token: "fake-token", isAdmin: false)
+    }
+    
+    func register(user: User) async throws {
+        if shouldFail {
+            throw NetworkError.serverError("Registration failed")
+        }
+    }
+}
